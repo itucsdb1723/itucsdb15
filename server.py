fe4b3aff6a9cad6b8cc4ad1bd0293a3d74c26086
agorq <alperengurses@outlook.com.tr>
2017-10-23 02:09:49 +0300

    Added ElephantSQL Functions

--- server.py
+++ server.py
@@ -1,17 +1,48 @@
 import datetime
 import os
+import json
+import re
+import psycopg2 as dbapi2
 
 from flask import Flask
+from flask import redirect
 from flask import render_template
+from flask.helpers import url_for
 
 
 app = Flask(__name__)
 
+def get_elephantsql_dsn(vcap_services):
+    """Returns the data source name for ElephantSQL."""
+    parsed = json.loads(vcap_services)
+    uri = parsed["elephantsql"][0]["credentials"]["uri"]
+    match = re.match('postgres://(.*?):(.*?)@(.*?)(:(\d+))?/(.*)', uri)
+    user, password, host, _, port, dbname = match.groups()
+    dsn = """user='{}' password='{}' host='{}' port={}
+             dbname='{}'""".format(user, password, host, port, dbname)
+    return dsn
 
 @app.route('/')
 def home_page():
     now = datetime.datetime.now()
     return render_template('home.html', current_time=now.ctime())
+
+@app.route('/initdb')
+def initialize_database():
+    with dbapi2.connect(app.config['dsn']) as connection:
+        cursor = connection.cursor()
+
+        query = """DROP TABLE IF EXIST COUNTER"""
+        cursor.execute(query)
+
+        query = """CREATE TABLE COUNTER (N INTEGER)"""
+        cursor.execute(query)
+
+        query = """INSERT INTO COUNTER (N) VALUES (0)"""
+        cursor.execute(query)
+
+        connection.commit()
+    return redirect(url_for('home_page')) 
 
 
 if __name__ == '__main__':
@@ -20,4 +51,11 @@ if __name__ == '__main__':
         port, debug = int(VCAP_APP_PORT), False
     else:
         port, debug = 5000, True
+
+    VCAP_SERVICES = os.getenv('VCAP_SERVICES')
+    if VCAP_SERVICES is not None:
+        app.config['dsn'] = get_elephantsql_dsn(VCAP_SERVICES)
+    else:
+        app.config['dsn'] = """user='vagrant' password='vagrant'
+                               host='localhost' port=54321 dbname='itucsdb'"""
     app.run(host='0.0.0.0', port=port, debug=debug)
