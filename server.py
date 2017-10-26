0a8cea46e2c38d2034b4b4eb33c268639f357d5d
Selim Eren Eryılmaz <selimereneryilmaz@gmail.com>
2017-10-26 15:24:43 +0300

    Added player table and route for player profiles

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import os
 import json
 import re
 import psycopg2 as dbapi2
+from datetime import date
 
 from flask import Flask
 from flask import redirect
@@ -23,18 +24,36 @@ def get_elephantsql_dsn(vcap_services):
     return dsn
 
 
-
-@app.route('/')
 @app.route('/home')
+@app.route('/')
 def home_page():
     return render_template('header.html', title="Dotabase", route="home") + \
            render_template('home.html') + \
            render_template('footer.html')
 
-
-
-
-
+@app.route('/player/<nick>')
+def player_profile(nick):
+    with dbapi2.connect(app.config['dsn']) as connection:
+        cursor = connection.cursor()
+        query = """ SELECT * FROM PLAYER WHERE p_nick=%s """
+        cursor.execute(query,[nick])
+        player_info = cursor.fetchall()[0]
+        connection.commit()
+    today=date.today()
+    info =  [('Name',player_info[3]+" "+player_info[4]),
+             ('Country',player_info[5]),
+             ('Birth Date',player_info[6]),
+             ('Age',today.year - player_info[6].year - ((today.month, today.day) < (player_info[6].month, player_info[6].day))),
+             ('Team','/Evil Geniuses'),
+             ('Solo MMR','/8108'),
+             ('Total Earnings','/$2,602,268')]
+    stats = [('DPC Points','/150'),
+             ('Played','/1'),
+             ('Qualified','/3'),
+             ('Team Rank','/2')]
+    return render_template('header.html', title="Dotabase", route="players") + \
+           render_template('profile.html', name=player_info[2], info=info, stats=stats) + \
+           render_template('footer.html')
 
 @app.route('/initdb')
 def initialize_database():
@@ -44,10 +63,29 @@ def initialize_database():
         query = """DROP TABLE IF EXISTS COUNTER"""
         cursor.execute(query)
 
-        query = """CREATE TABLE COUNTER (N INTEGER)"""
+        query = """DROP TABLE IF EXISTS PLAYER"""
         cursor.execute(query)
 
-        query = """INSERT INTO COUNTER (N) VALUES (0)"""
+        query = """CREATE TABLE PLAYER (
+        p_id SERIAL PRIMARY KEY,
+        p_accountid INTEGER UNIQUE,
+        p_nick VARCHAR(20) NOT NULL UNIQUE,
+        p_name VARCHAR(60),
+        p_surname VARCHAR(60),
+        p_country VARCHAR(2),
+        p_birth DATE,
+        p_mmr INTEGER
+        )"""
+        cursor.execute(query)
+
+        query = """INSERT INTO PLAYER (p_nick,p_name,p_surname,p_country,p_birth)
+        VALUES ('SumaiL','Syed Sumail','Hassan','PK','1999-02-13')"""
+        cursor.execute(query)
+        query = """INSERT INTO PLAYER (p_nick,p_name,p_surname,p_country,p_birth)
+        VALUES ('MATUMBAMAN','Lasse Aukusti','Urpalainen','FI','1995-03-03')"""
+        cursor.execute(query)
+        query = """INSERT INTO PLAYER (p_nick,p_name,p_surname,p_country,p_birth)
+        VALUES ('Miracle-','Amer','Al-Barqawi','JO','1997-06-20')"""
         cursor.execute(query)
 
         connection.commit()
