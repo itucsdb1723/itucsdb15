ef82e25257a0b02493e19ca2c797148445819df2
Selim Eren Eryılmaz <selimereneryilmaz@gmail.com>
2017-11-02 02:15:38 +0300

    Improved player profiles

--- server.py
+++ server.py
@@ -10,6 +10,9 @@ from flask import redirect
 from flask import render_template
 from flask.helpers import url_for
 
+countrieslist = """{"BD": "Bangladesh", "BE": "Belgium", "BF": "Burkina Faso", "BG": "Bulgaria", "BA": "Bosnia and Herzegovina", "BB": "Barbados", "WF": "Wallis and Futuna", "BL": "Saint Barthelemy", "BM": "Bermuda", "BN": "Brunei", "BO": "Bolivia", "BH": "Bahrain", "BI": "Burundi", "BJ": "Benin", "BT": "Bhutan", "JM": "Jamaica", "BV": "Bouvet Island", "BW": "Botswana", "WS": "Samoa", "BQ": "Bonaire, Saint Eustatius and Saba ", "BR": "Brazil", "BS": "Bahamas", "JE": "Jersey", "BY": "Belarus", "BZ": "Belize", "RU": "Russia", "RW": "Rwanda", "RS": "Serbia", "TL": "East Timor", "RE": "Reunion", "TM": "Turkmenistan", "TJ": "Tajikistan", "RO": "Romania", "TK": "Tokelau", "GW": "Guinea-Bissau", "GU": "Guam", "GT": "Guatemala", "GS": "South Georgia and the South Sandwich Islands", "GR": "Greece", "GQ": "Equatorial Guinea", "GP": "Guadeloupe", "JP": "Japan", "GY": "Guyana", "GG": "Guernsey", "GF": "French Guiana", "GE": "Georgia", "GD": "Grenada", "GB": "United Kingdom", "GA": "Gabon", "SV": "El Salvador", "GN": "Guinea", "GM": "Gambia", "GL": "Greenland", "GI": "Gibraltar", "GH": "Ghana", "OM": "Oman", "TN": "Tunisia", "JO": "Jordan", "HR": "Croatia", "HT": "Haiti", "HU": "Hungary", "HK": "Hong Kong", "HN": "Honduras", "HM": "Heard Island and McDonald Islands", "VE": "Venezuela", "PR": "Puerto Rico", "PS": "Palestinian Territory", "PW": "Palau", "PT": "Portugal", "SJ": "Svalbard and Jan Mayen", "PY": "Paraguay", "IQ": "Iraq", "PA": "Panama", "PF": "French Polynesia", "PG": "Papua New Guinea", "PE": "Peru", "PK": "Pakistan", "PH": "Philippines", "PN": "Pitcairn", "PL": "Poland", "PM": "Saint Pierre and Miquelon", "ZM": "Zambia", "EH": "Western Sahara", "EE": "Estonia", "EG": "Egypt", "ZA": "South Africa", "EC": "Ecuador", "IT": "Italy", "VN": "Vietnam", "SB": "Solomon Islands", "ET": "Ethiopia", "SO": "Somalia", "ZW": "Zimbabwe", "SA": "Saudi Arabia", "ES": "Spain", "ER": "Eritrea", "ME": "Montenegro", "MD": "Moldova", "MG": "Madagascar", "MF": "Saint Martin", "MA": "Morocco", "MC": "Monaco", "UZ": "Uzbekistan", "MM": "Myanmar", "ML": "Mali", "MO": "Macao", "MN": "Mongolia", "MH": "Marshall Islands", "MK": "Macedonia", "MU": "Mauritius", "MT": "Malta", "MW": "Malawi", "MV": "Maldives", "MQ": "Martinique", "MP": "Northern Mariana Islands", "MS": "Montserrat", "MR": "Mauritania", "IM": "Isle of Man", "UG": "Uganda", "TZ": "Tanzania", "MY": "Malaysia", "MX": "Mexico", "IL": "Israel", "FR": "France", "IO": "British Indian Ocean Territory", "SH": "Saint Helena", "FI": "Finland", "FJ": "Fiji", "FK": "Falkland Islands", "FM": "Micronesia", "FO": "Faroe Islands", "NI": "Nicaragua", "NL": "Netherlands", "NO": "Norway", "NA": "Namibia", "VU": "Vanuatu", "NC": "New Caledonia", "NE": "Niger", "NF": "Norfolk Island", "NG": "Nigeria", "NZ": "New Zealand", "NP": "Nepal", "NR": "Nauru", "NU": "Niue", "CK": "Cook Islands", "XK": "Kosovo", "CI": "Ivory Coast", "CH": "Switzerland", "CO": "Colombia", "CN": "China", "CM": "Cameroon", "CL": "Chile", "CC": "Cocos Islands", "CA": "Canada", "CG": "Republic of the Congo", "CF": "Central African Republic", "CD": "Democratic Republic of the Congo", "CZ": "Czech Republic", "CY": "Cyprus", "CX": "Christmas Island", "CR": "Costa Rica", "CW": "Curacao", "CV": "Cape Verde", "CU": "Cuba", "SZ": "Swaziland", "SY": "Syria", "SX": "Sint Maarten", "KG": "Kyrgyzstan", "KE": "Kenya", "SS": "South Sudan", "SR": "Suriname", "KI": "Kiribati", "KH": "Cambodia", "KN": "Saint Kitts and Nevis", "KM": "Comoros", "ST": "Sao Tome and Principe", "SK": "Slovakia", "KR": "South Korea", "SI": "Slovenia", "KP": "North Korea", "KW": "Kuwait", "SN": "Senegal", "SM": "San Marino", "SL": "Sierra Leone", "SC": "Seychelles", "KZ": "Kazakhstan", "KY": "Cayman Islands", "SG": "Singapore", "SE": "Sweden", "SD": "Sudan", "DO": "Dominican Republic", "DM": "Dominica", "DJ": "Djibouti", "DK": "Denmark", "VG": "British Virgin Islands", "DE": "Germany", "YE": "Yemen", "DZ": "Algeria", "US": "United States", "UY": "Uruguay", "YT": "Mayotte", "UM": "United States Minor Outlying Islands", "LB": "Lebanon", "LC": "Saint Lucia", "LA": "Laos", "TV": "Tuvalu", "TW": "Taiwan", "TT": "Trinidad and Tobago", "TR": "Turkey", "LK": "Sri Lanka", "LI": "Liechtenstein", "LV": "Latvia", "TO": "Tonga", "LT": "Lithuania", "LU": "Luxembourg", "LR": "Liberia", "LS": "Lesotho", "TH": "Thailand", "TF": "French Southern Territories", "TG": "Togo", "TD": "Chad", "TC": "Turks and Caicos Islands", "LY": "Libya", "VA": "Vatican", "VC": "Saint Vincent and the Grenadines", "AE": "United Arab Emirates", "AD": "Andorra", "AG": "Antigua and Barbuda", "AF": "Afghanistan", "AI": "Anguilla", "VI": "U.S. Virgin Islands", "IS": "Iceland", "IR": "Iran", "AM": "Armenia", "AL": "Albania", "AO": "Angola", "AQ": "Antarctica", "AS": "American Samoa", "AR": "Argentina", "AU": "Australia", "AT": "Austria", "AW": "Aruba", "IN": "India", "AX": "Aland Islands", "AZ": "Azerbaijan", "IE": "Ireland", "ID": "Indonesia", "UA": "Ukraine", "QA": "Qatar", "MZ": "Mozambique"}"""
+
+countries = json.loads(countrieslist)
 
 app = Flask(__name__)
 
@@ -35,20 +38,20 @@ def home_page():
 def player_profile(nick):
     with dbapi2.connect(app.config['dsn']) as connection:
         cursor = connection.cursor()
-        query = """ SELECT * FROM PLAYER WHERE p_nick=%s """
+        query = """ SELECT * FROM PLAYER LEFT JOIN (SELECT t_id,t_name FROM TEAM) AS TEAM ON PLAYER.t_id=TEAM.t_id WHERE p_nick=%s """
         cursor.execute(query,[nick])
         player_info = cursor.fetchall()[0]
-        query2 = """ SELECT * FROM ROSTER JOIN TEAM ON ROSTER.t_id=TEAM.t_id WHERE p_id=%s ORDER BY join_date ASC"""
+        query2 = """ SELECT t_name,join_date,leave_date,position,is_captain FROM ROSTER JOIN TEAM ON ROSTER.t_id=TEAM.t_id WHERE p_id=%s ORDER BY join_date ASC"""
         cursor.execute(query2,[player_info[0]])
         history = cursor.fetchall()
         connection.commit()
     today=date.today()
     info =  [('Name',player_info[3]+" "+player_info[4]),
-             ('Country',player_info[5]),
+             ('Country',countries[player_info[5]]),
              ('Birth Date',player_info[6]),
              ('Age',today.year - player_info[6].year - ((today.month, today.day) < (player_info[6].month, player_info[6].day))),
-             ('Team','/Evil Geniuses'),
-             ('Solo MMR','/8108'),
+             ('Team',player_info[10]),
+             ('Solo MMR',player_info[7]),
              ('Total Earnings','/$2,602,268')]
     stats = [('DPC Points','/150'),
              ('Played','/1'),
@@ -87,16 +90,22 @@ def initialize_database():
     with dbapi2.connect(app.config['dsn']) as connection:
         cursor = connection.cursor()
 
-        query = """DROP TABLE IF EXISTS COUNTER"""
+        query = """DROP TABLE IF EXISTS COUNTER CASCADE"""
+        cursor.execute(query)
+        query = """DROP TABLE IF EXISTS ROSTER CASCADE"""
+        cursor.execute(query)
+        query = """DROP TABLE IF EXISTS TEAM CASCADE"""
+        cursor.execute(query)
+        query = """DROP TABLE IF EXISTS PLAYER CASCADE"""
         cursor.execute(query)
 
-        query = """DROP TABLE IF EXISTS PLAYER"""
-        cursor.execute(query)
-
-        query = """DROP TABLE IF EXISTS TEAM"""
-        cursor.execute(query)
-
-        query = """DROP TABLE IF EXISTS ROSTER"""
+        query = """ CREATE TABLE TEAM(
+        t_id SERIAL PRIMARY KEY,
+        t_name VARCHAR(60),
+        t_tag VARCHAR(10),
+        t_region INTEGER,
+        t_created DATE
+        )"""
         cursor.execute(query)
 
         query = """CREATE TABLE PLAYER (
@@ -107,26 +116,9 @@ def initialize_database():
         p_surname VARCHAR(60),
         p_country VARCHAR(2),
         p_birth DATE,
-        p_mmr INTEGER
-        )"""
-        cursor.execute(query)
-
-        query = """INSERT INTO PLAYER (p_nick,p_name,p_surname,p_country,p_birth)
-        VALUES ('SumaiL','Syed Sumail','Hassan','PK','1999-02-13')"""
-        cursor.execute(query)
-        query = """INSERT INTO PLAYER (p_nick,p_name,p_surname,p_country,p_birth)
-        VALUES ('MATUMBAMAN','Lasse Aukusti','Urpalainen','FI','1995-03-03')"""
-        cursor.execute(query)
-        query = """INSERT INTO PLAYER (p_nick,p_name,p_surname,p_country,p_birth)
-        VALUES ('Miracle-','Amer','Al-Barqawi','JO','1997-06-20')"""
-        cursor.execute(query)
-
-        query = """ CREATE TABLE TEAM(
-        t_id SERIAL PRIMARY KEY,
-        t_name VARCHAR(60),
-        t_tag VARCHAR(10),
-        t_region INTEGER,
-        t_created DATE
+        t_id INTEGER,
+        p_mmr INTEGER,
+        FOREIGN KEY(t_id) REFERENCES TEAM(t_id) ON DELETE CASCADE
         )"""
         cursor.execute(query)
 
@@ -140,6 +132,16 @@ def initialize_database():
         VALUES ('Team Liquid','Liquid ',3,'2012-12-06')"""
         cursor.execute(query)
 
+        query = """INSERT INTO PLAYER (p_nick,p_name,p_surname,p_country,p_birth,p_mmr,t_id)
+        VALUES ('SumaiL','Syed Sumail','Hassan','PK','1999-02-13','8140',(SELECT t_id FROM TEAM WHERE t_name='Evil Geniuses'))"""
+        cursor.execute(query)
+        query = """INSERT INTO PLAYER (p_nick,p_name,p_surname,p_country,p_birth,p_mmr,t_id)
+        VALUES ('MATUMBAMAN','Lasse Aukusti','Urpalainen','FI','1995-03-03','9421',(SELECT t_id FROM TEAM WHERE t_name LIKE 'Team Liquid'))"""
+        cursor.execute(query)
+        query = """INSERT INTO PLAYER (p_nick,p_name,p_surname,p_country,p_birth)
+        VALUES ('Miracle-','Amer','Al-Barqawi','JO','1997-06-20')"""
+        cursor.execute(query)
+
         query = """ CREATE TABLE ROSTER(
         p_id INTEGER NOT NULL,
         t_id INTEGER NOT NULL,
@@ -147,8 +149,8 @@ def initialize_database():
         leave_date DATE,
         position INTEGER,
         is_captain BOOLEAN NOT NULL,
-        FOREIGN KEY(p_id) REFERENCES PLAYER(p_id),
-        FOREIGN KEY(t_id) REFERENCES TEAM(t_id)
+        FOREIGN KEY(p_id) REFERENCES PLAYER(p_id) ON DELETE CASCADE,
+        FOREIGN KEY(t_id) REFERENCES TEAM(t_id) ON DELETE CASCADE
         )"""
         cursor.execute(query)
         query = """ INSERT INTO ROSTER (p_id, t_id , join_date, position,is_captain)
